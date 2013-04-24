3abdc37499055e24a5e9ef66a75d329334d7450c
krabik <tom.wrzonski@gmail.com>
2013-04-24 17:59:08 +0300

    add print

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-  
+            Console.WriteLine("done!");
             Console.ReadKey();
             
         }
